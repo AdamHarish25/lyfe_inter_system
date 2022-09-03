7fd7dc141a7a09036a396e1c8ad9feb4df5f706d
adam <adamharits25@gmail.com>
2022-09-03 15:09:57 +0700

    Lyfe_Inter_System 1.7

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ScreenWarn from "./Components/ScreenWarning";
 
 function App() {
   return (
-    <div id="App" className="h-auto w-auto overflow-x-hidden lg:overflow-x-auto">
+    <div id="App" className="h-auto w-auto overflow-x-hidden xl:overflow-x-auto">
       {/* <Login /> */}
       <AdminDashboard visibility="hidden lg:grid" />
       <ScreenWarn visibility="block lg:hidden" />
--- src/Components/AdminDashboard.js
+++ src/Components/AdminDashboard.js
@@ -8,7 +8,7 @@ export default function AdminDashboard({ visibility }) {
 
 
     return (
-        <div className={`${visibility} w-auto h-auto xl:h-screenLg py-5 pr-5 bg-[#f5e4d7] grid-cols-SL gap-5`}>
+        <div className={`${visibility} w-auto h-auto py-5 pr-5 bg-[#f5e4d7] grid-cols-SL gap-5`}>
 
             <DashSidebar />
 
